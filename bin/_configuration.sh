f89eab512a2873141acca84b34dfc1560a6a1003
Atousss <florent.zoro@gmail.com>
2021-07-09 14:00:52 +0200

    Structure de base du projet

---
+++ bin/_configuration.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+MYSQL_USER="explorateur"
+MYSQL_PASSWORD="Ereul9Aeng"
+MYSQL_HOST="localhost"
+WORDPRESS_BDD="Magic-Trucks"
+
+WORDPRESS_URL="http://localhost/apotheose/magic-trucks/public"
+
+WORDPRESS_SITE_NAME="Magic-Trucks"
+WORDPRESS_ADMIN_NAME="admin"
+WORDPRESS_ADMIN_PASSWORD="admin"
+WORDPRESS_ADMIN_EMAIL="admin@mail.com"
+
+# optionnal configurations
+WORDPRESS_FOLDER="wp"
+WORDPRESS_CONTENT_FOLDER="content"
+WORDPRESS_TABLE_PREFIX="mt_"
